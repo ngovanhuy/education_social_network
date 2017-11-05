baf696ec8437d7bf2e71ec5761e44f301f0fa052
nkn6294live <nkn6294@live.co.uk>
2017-11-05 22:54:50 +0700

    uncommen debug connectToDB

--- service/project/application/manager.js
+++ service/project/application/manager.js
@@ -40,9 +40,9 @@ function start() {
 }
 async function connectToDB() {
     try {
-        let c = "mongodb://192.168.1.17:27017/education_social_network";
-        await mongoose.connect(c, {
-        // await mongoose.connect(exports.connectString, {
+        // let c = "mongodb://192.168.1.17:27017/education_social_network";
+        // await mongoose.connect(c, {
+        await mongoose.connect(exports.connectString, {//loi khi debug -> can fix.
             useMongoClient: true,
             // server: {auto_reconnect: true}//Default reconnect in 30s.
             reconnectTries: Number.MAX_VALUE,
