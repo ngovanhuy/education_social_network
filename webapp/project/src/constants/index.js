7f80cc9dd9dd8e9fc9dadaae2c261b52cf83e81f
huynv <ngovanhuy.cntt2@gmail.com>
2018-03-20 17:08:21 +0700

    dockerizing project

--- webapp/project/src/constants/index.js
+++ webapp/project/src/constants/index.js
@@ -6,7 +6,7 @@ export * from './eventConstants';
 export * from './postConstants';
 export * from './announcementConstants';
 export * from './settingConstants';
-export const DOMAIN_SERVICE = 'http://localhost:3000'
+export const DOMAIN_SERVICE = 'http://localhost:9998'
 export const FB_APP_ID = '1383813771747393'
 export const FB_APP_SECRET = 'adde5c9ed1b91f8e23fb3f8d5ed994cb'
 export const DOMAIN_FB_SERVICE = 'https://graph.facebook.com'
